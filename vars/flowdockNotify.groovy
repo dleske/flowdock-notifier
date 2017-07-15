622e1bc86f42018ecf5147b74c30643dcb3e0499
Drew Leske <drew.leske@computecanada.ca>
2017-07-15 07:10:05 +0000

    And again

--- vars/flowdockNotify.groovy
+++ vars/flowdockNotify.groovy
@@ -4,81 +4,68 @@ import java.net.URLEncoder
 import hudson.model.Result
 
 def call(script, apiToken, tags = '') {
+
     tags = tags.replaceAll("\\s","")
+
+    def flowdockURL = "https://api.flowdock.com/v1/messages/team_inbox/${apiToken}"
+    def fromAddress = 'noreply+jenkins@computecanada.ca'
+
     // build status of null means successful
     def buildStatus =  script.currentBuild.result ? script.currentBuild.result : 'SUCCESS'
-    def subject = "${script.env.JOB_BASE_NAME} build ${script.currentBuild.displayName.replaceAll("#", "")}"
-    def fromAddress = ''
 
-    println("Build status: ${buildStatus}")
+    // create subject and update with build status
+    def subject = "${script.env.JOB_BASE_NAME} build ${script.currentBuild.displayName.replaceAll("#", "")}"
 
     switch (buildStatus) {
       case 'SUCCESS':
         def prevResult = script.currentBuild.getPreviousBuild() != null ? script.currentBuild.getPreviousBuild().getResult() : null;
         if (Result.FAILURE.toString().equals(prevResult) || Result.UNSTABLE.toString().equals(prevResult)) {
           subject += ' was fixed'
-          fromAddress = 'build+ok@flowdock.com'
           break
         }
         subject += ' was successful'
-        fromAddress = 'build+ok@flowdock.com'
         break
       case 'FAILURE':
         subject += ' failed'
-        fromAddress = 'build+fail@flowdock.com'
         break
       case 'UNSTABLE':
         subject += ' was unstable'
-        fromAddress = 'build+fail@flowdock.com'
         break
       case 'ABORTED':
         subject += ' was aborted'
-        fromAddress = 'build+fail@flowdock.com'
         break
       case 'NOT_BUILT':
         subject += ' was not built'
-        fromAddress = 'build+fail@flowdock.com'
         break
       case 'FIXED':
         subject = ' was fixed'
-        fromAddress = 'build+ok@flowdock.com'
         break
     }
 
-    println("Subject: ${subject}")
-
+    // build message
     def content = """<h3>${script.env.JOB_BASE_NAME}</h3>
       Build: ${script.currentBuild.displayName}<br />
       Result: <strong>${buildStatus}<br />
       URL: <a href="${script.env.BUILD_URL}">${script.currentBuild.fullDisplayName}</a><br />"""
 
-    //StringBuilder content = new StringBuilder();
-    //content.append("<h3>").append(script.env.JOB_BASE_NAME).append("</h3>");
-    //content.append("Build: ").append(script.currentBuild.displayName).append("<br />");
-    //content.append("Result: <strong>").append(buildStatus).append("</strong><br />");
-    //content.append("URL: <a href=\"").append(script.env.BUILD_URL).append("\">").append(script.currentBuild.fullDisplayName).append("</a>").append("<br />");
-
-    println("Content: ${content}")
-
-    def flowdockURL = "https://api.flowdock.com/v1/messages/team_inbox/${apiToken}"
+    // build payload
     def payload = JsonOutput.toJson([source : "Jenkins",
                                      project : script.env.JOB_BASE_NAME,
                                      from_address: fromAddress,
-                                     from_name: 'CI',
+                                     from_name: 'Jenkins',
                                      subject: subject,
                                      tags: tags,
                                      content: content,
                                      link: script.env.BUILD_URL
                                      ])
 
-    println("Payload: ${payload}")
-
+    // craft and send the request
     def post = new URL(flowdockURL).openConnection();
     post.setRequestMethod("POST");
     post.setDoOutput(true);
     post.setRequestProperty("Content-Type", "application/json")
     post.getOutputStream().write(payload.getBytes("UTF-8"));
     def postRC = post.getResponseCode();
-    println("Received response: " + postRC);
 
+    println("Response received from Flowdock API: " + postRC);
 }
